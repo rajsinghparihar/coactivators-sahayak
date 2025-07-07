d17ec886296e91fdb58520945bbd2bde044aede3
rajsinghparihar <rajsingh212001@gmail.com>
2025-07-07 19:54:01 +0530

    fix gcloud setup script

--- gcloud-setup.sh
+++ gcloud-setup.sh
@@ -1,4 +1,4 @@
-
+mkdir gcloud-setup;
 cd gcloud-setup;
 wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-darwin-arm.tar.gz;
 tar -xf google-cloud-cli-darwin-arm.tar.gz;
